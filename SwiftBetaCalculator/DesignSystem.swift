a9078156c4ebe8069413ece1f1be4fa83d60693f
Felipe Flores <jorge.flores.m@tecsup.edu.pe>
2023-05-03 23:10:40 -0500

    lab07

---
+++ SwiftBetaCalculator/DesignSystem.swift
@@ -0,0 +1,24 @@
+//
+//  DesignSystem.swift
+//  SwiftBetaCalculator
+//
+//  Created by Home on 16/8/21.
+//
+
+import Foundation
+import SwiftUI
+
+let customOrange = Color(red: 254/255,
+                         green: 159/255,
+                         blue: 6/255,
+                         opacity: 1.0)
+
+let customLightGray = Color(red: 165/255,
+                            green: 165/255,
+                            blue: 165/255,
+                            opacity: 1.0)
+
+let customDarkGray = Color(red: 51/255,
+                           green: 51/255,
+                           blue: 51/255,
+                           opacity: 1.0)
---
+++ SwiftBetaCalculator/SwiftBetaCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBetaCalculatorApp.swift
+//  SwiftBetaCalculator
+//
+//  Created by Home on 20/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBetaCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
